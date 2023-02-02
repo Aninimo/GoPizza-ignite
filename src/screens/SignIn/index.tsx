7a2f566580eb8acd10bb0edf8cd86c2102e12544
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-02-02 15:34:02 -0200

    Create index.tsx

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import { useAuth } from '@hooks/auth';
+
+import brandImg from '@assets/images/brand.png';
+
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+} from './styles';
+
+export function SignIn() {
+  const { signIn, signOut, forgotPassword, isLogging } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSignIn() {
+    signIn(email, password);
+  }
+
+  function handleForgotPassword() {
+    forgotPassword(email);
+  }
+
+  return (
+    <Container>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
+          <Brand source={brandImg} />
+
+          <Title>Login</Title>
+
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+            onChangeText={setEmail}
+          />
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+
+          <ForgotPasswordButton onPress={handleForgotPassword}>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title="Entrar"
+            type="secondary"
+            onPress={handleSignIn}
+            isLoading={isLogging}
+          />
+        </Content>
+      </KeyboardAvoidingView>
+    </Container>
+  );
+}
