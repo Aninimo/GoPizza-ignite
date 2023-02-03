895d18f6ccd57e6f88fa03465fc0605e4c8e18dd
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-02-02 22:22:29 -0200

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+};
